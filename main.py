9290e43a422271540eccfc7c803ce1b9034b02d4
samxele <samelenj2003@gmail.com>
2023-08-30 23:00:13 -0500

    While loop and nonfollowers
    While loop created so that program can run on a loop until told to stop. Can also see those that are not following back.

--- main.py
+++ main.py
@@ -1,44 +1,81 @@
-from instabot import Bot
+from instabot import Bot, utils
 myBot = Bot()
 
 myBot = Bot(max_likes_to_like=1000)
 myBot.login(username="samspythonbot", password="Samisawesome12")
+menuChoice = 0
 
 print("Welcome to Sam's Instagram Bot")
-print("What would you like to do?")
-menuChoice = int(input("1.Follow a user\n2.Like a post\n3.Comment on a post\n4.Post a photo\n5.Like & comment on post\n"
-                       "6.Send a DM\n"))
 
-if menuChoice == 1:
-    followAcc = input("Enter the name of the account you would like to follow: ")
-    myBot.follow(followAcc)
-elif menuChoice == 2:
-    mediaLink = input("Enter link to the post: ")
-    media = myBot.get_media_id_from_link(mediaLink)
-    myBot.like(media)
-elif menuChoice == 3:
-    mediaLink = input("Enter link to the post: ")
-    media = myBot.get_media_id_from_link(mediaLink)
-    userComment = input("Enter comment: ")
-    myBot.comment(media, userComment)
-elif menuChoice == 4:
-    imgName = input("Enter the name of the image you would like to post. Include .jpg/.jpeg/: ")
-    capChoice = input("Would you like to post a caption? Y/N: ").upper()
-    if capChoice == "Y":
-        userComm = input("Enter caption: ")
-        myBot.upload_photo(imgName,caption=userComm)
-    elif capChoice == "N":
-        myBot.upload_photo(imgName)
-elif menuChoice == 5:
-    mediaLink = input("Enter link to the post: ")
-    media = myBot.get_media_id_from_link(mediaLink)
-    userComment = input("Enter comment: ")
-    myBot.comment(media, userComment)
-    myBot.like(media)
-elif menuChoice == 6:
-    dmAcc = input("Enter account you would like to send a DM to: ")
-    dmMess = input("Enter message: ")
-    myBot.send_message(dmMess, dmAcc)
+while menuChoice != 100:
+    print("What would you like to do?")
+    menuChoice = int(
+        input("1.Follow a user\n2.Like a post\n3.Comment on a post\n4.Post a photo\n5.Like & comment on post\n"
+              "6.Send a DM\n7.See those that aren't following back\n100.Logout and exit program\n"))
 
-myBot.logout()
-exit()
+    # Follows User
+    if menuChoice == 1:
+        followAcc = input("Enter the name of the account you would like to follow: ")
+        myBot.follow(followAcc)
+
+    # Likes a post
+    elif menuChoice == 2:
+        mediaLink = input("Enter link to the post: ")
+        media = myBot.get_media_id_from_link(mediaLink)
+        myBot.like(media)
+
+    # Comments on a post
+    elif menuChoice == 3:
+        mediaLink = input("Enter link to the post: ")
+        media = myBot.get_media_id_from_link(mediaLink)
+        userComment = input("Enter comment: ")
+        myBot.comment(media, userComment)
+
+    # Posts a photo
+    elif menuChoice == 4:
+        imgName = input("Enter the name of the image you would like to post. Include .jpg/.jpeg/: ")
+        capChoice = input("Would you like to post a caption? Y/N: ").upper()
+        if capChoice == "Y":
+            userComm = input("Enter caption: ")
+            myBot.upload_photo(imgName, caption=userComm)
+        elif capChoice == "N":
+            myBot.upload_photo(imgName)
+
+    # Like and comment on a post
+    elif menuChoice == 5:
+        mediaLink = input("Enter link to the post: ")
+        media = myBot.get_media_id_from_link(mediaLink)
+        userComment = input("Enter comment: ")
+        myBot.comment(media, userComment)
+        myBot.like(media)
+
+    # Send a dm to a user
+    elif menuChoice == 6:
+        dmAcc = input("Enter account you would like to send a DM to: ")
+        picChoice = input("Would you like to send an immage? Y/N: ").upper()
+        if picChoice == "Y":
+            dmMess = input("Enter message: ")
+            dmPic = input("Enter image name")
+            myBot.send_message(dmMess, dmAcc)
+            myBot.send_photo(dmAcc, dmPic)
+        if picChoice == "N":
+            dmMess = input("Enter message: ")
+            myBot.send_message(dmMess, dmAcc)
+
+    # See those who aren't following you back
+    elif menuChoice == 7:
+        file = utils.file("config/non-followers.txt")
+        nonFollowers = set(myBot.following) - set(myBot.followers) - myBot.friends_file.set
+        nonFollowers = list(nonFollowers)
+        nonFollowersNames = []
+
+        for user in nonFollowers:
+            name = myBot.get_username_from_user_id(user)
+            nonFollowersNames.append(name)
+            print(name)
+            myBot.small_delay()
+        file.save_list(nonFollowersNames)
+
+if menuChoice == 100:
+    myBot.logout()
+    exit()
